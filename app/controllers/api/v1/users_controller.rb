9bfee0223b4cc6f7e7546346ada106623cd7a2fc
Costa Nonato <costa@nonato.com>
2017-04-12 13:09:50 +0000

    Adding create action to the users controller with tests

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -9,4 +9,20 @@ class Api::V1::UsersController < ApplicationController
       head 404
     end
   end
+
+  def create
+    user = User.new(user_params)
+
+    if user.save
+      render json: user, status: 201
+    else
+      render json: { errors: user.errors }, status: 422
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
 end
--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -33,5 +33,39 @@ RSpec.describe 'Users API', type: :request do
     end
 
   end
+
+
+  describe 'POST /users' do
+    before do
+      headers = { 'Accept' => 'application/vnd.taskmanager.v1' }
+      post '/users', params: { user: user_params }, headers: headers
+    end
+
+    context 'when the request params are valid' do
+      let(:user_params) { attributes_for(:user) }
+
+      it 'returns status code 201' do    
+        expect(response).to have_http_status(201)    
+      end
+
+      it 'returns json data for the created user' do
+        user_response = JSON.parse(response.body)
+        expect(user_response['email']).to eq(user_params[:email])
+      end
+    end
+
+    context 'when the request params are invalid' do
+      let(:user_params) { attributes_for(:user, email: 'invalid_email@') }
+
+      it 'returns status code 422' do
+        expect(response).to have_http_status(422)
+      end
+
+      it 'returns the json data for the erros' do
+        user_response = JSON.parse(response.body)
+        expect(user_response).to have_key('errors')
+      end
+    end
+  end
   
 end
