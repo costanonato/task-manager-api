e1360f94d5bf12cfd425009ebfc8abb19b50cd61
Costa Nonato <costa@nonato.com>
2017-10-14 22:24:30 +0000

    adding new I18n time format

--- app/serializers/api/v2/task_serializer.rb
+++ app/serializers/api/v2/task_serializer.rb
@@ -1,6 +1,6 @@
 class Api::V2::TaskSerializer < ActiveModel::Serializer
   attributes :id, :title, :description, :done, :deadline, :user_id, :created_at, :updated_at,
-              :short_description, :is_late
+              :short_description, :is_late, :deadline_to_br
 
 
   def short_description
@@ -9,6 +9,10 @@ class Api::V2::TaskSerializer < ActiveModel::Serializer
 
   def is_late
     Time.current > object.deadline if object.deadline.present?
+  end
+
+  def deadline_to_br
+    I18n.l(object.deadline, format: :datetime) if object.deadline.present?
   end
 
   belongs_to :user
