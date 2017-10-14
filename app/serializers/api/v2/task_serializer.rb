fc4fa8a5499830a1d503f13a152346c21797f4a2
Costa Nonato <costa@nonato.com>
2017-10-14 22:01:06 +0000

    fixing task_serializer

--- app/serializers/api/v2/task_serializer.rb
+++ app/serializers/api/v2/task_serializer.rb
@@ -4,7 +4,7 @@ class Api::V2::TaskSerializer < ActiveModel::Serializer
 
 
   def short_description
-    object.description[0..40]
+    object.description[0..40] if object.description.present?
   end
 
   def is_late
