4f92b3b5f6928c25daccb3afaa0b6cc546c6257c
Costa Nonato <costa@nonato.com>
2018-04-02 22:28:10 +0000

    removing 'api' route constraint

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/'  do
+  namespace :api, defaults: { format: :json }, path: '/'  do
     namespace :v1, path: '/', constraints: ApiVersionConstraint.new(version: 1) do
       resources :users, only: [:show, :create, :update, :destroy]
       resources :sessions, only: [:create, :destroy]
